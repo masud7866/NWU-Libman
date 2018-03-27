c6e5f4cfcc8e83f4d694170d86ad2bd29322d3d9
Minhaz Rahman <iamtusharkhan@gmail.com>
2018-03-28 02:50:50 +0600

    login css load fixed

--- views/dashboard.php
+++ views/dashboard.php
@@ -25,34 +25,36 @@ class dashboard extends templates\main_template
         <div class="container">
             <div class="row">
 
-                <div class="col-lg-3 dashstyle">
-                    <h3 class="dash-title">Total Books</h3>
-                    <h1 class="dash-score">4000</h1>
-                </div>
+                <div class="col-lg-12">
+                    <div class="col-lg-3 dashstyle">
+                        <h3 class="dash-title">Total Books</h3>
+                        <h1 class="dash-score">4000</h1>
+                    </div>
 
-                <div class="col-lg-3 dashstyle">
-                    <h3 class="dash-title">Total Members</h3>
-                    <h1 class="dash-score">2500</h1>
-                </div>
+                    <div class="col-lg-3 dashstyle">
+                        <h3 class="dash-title">Total Members</h3>
+                        <h1 class="dash-score">2500</h1>
+                    </div>
 
-                <div class="col-lg-3 dashstyle">
-                    <h3 class="dash-title">Total Stuff</h3>
-                    <h1 class="dash-score">5</h1>
-                </div>
+                    <div class="col-lg-3 dashstyle">
+                        <h3 class="dash-title">Total Stuff</h3>
+                        <h1 class="dash-score">5</h1>
+                    </div>
 
-                <div class="col-lg-3 dashstyle">
-                    <h3 class="dash-title">Total Manager</h3>
-                    <h1 class="dash-score">2</h1>
-                </div>
+                    <div class="col-lg-3 dashstyle">
+                        <h3 class="dash-title">Total Manager</h3>
+                        <h1 class="dash-score">2</h1>
+                    </div>
 
-                <div class="col-lg-3 dashstyle">
-                    <h3 class="dash-title">Stock Warning!</h3>
-                    <h1 class="dash-score">20</h1>
-                </div>
+                    <div class="col-lg-3 dashstyle">
+                        <h3 class="dash-title">Stock Warning!</h3>
+                        <h1 class="dash-score">20</h1>
+                    </div>
 
-                <div class="col-lg-3 dashstyle">
-                    <h3 class="dash-title">Active Borrowings Member</h3>
-                    <h1 class="dash-score">2000</h1>
+                    <div class="col-lg-3 dashstyle">
+                        <h3 class="dash-title">Active Borrowings Member</h3>
+                        <h1 class="dash-score">2000</h1>
+                    </div>
                 </div>
 
             </div>
